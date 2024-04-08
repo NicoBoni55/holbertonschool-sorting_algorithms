28cccafa903ffbc70cf15b41a434dc1ae4b1e87c
nachoneca <8666@holbertonstudents.com>
2024-04-08 21:58:57 +0000

    Fix betty-coding-style on 2-selection_sort.c

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -9,13 +9,14 @@ void selection_sort(int *array, size_t size)
 {
 	size_t min_index, i, j;
 	int temp;
-	if (array == NULL || size < 2) 
+
+	if (array == NULL || size < 2)
 		return;
-	for (i = 0; i < size - 1; i++) 
+	for (i = 0; i < size - 1; i++)
 	{
 		min_index = i;
 
-	for (j = i + 1; j < size; j++) 
+	for (j = i + 1; j < size; j++)
 	{
 		if (array[j] < array[min_index])
 			min_index = j;
